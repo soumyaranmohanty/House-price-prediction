9048714b8a696c96cd23d3c0d8df680165e36141
soumyaranmohanty <soumyaranmohanty@gmail.com>
2025-11-10 02:27:52 +0530

    House price prediction app

---
+++ app.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pickle
+import json
+import pandas as pd
+
+def predict_price(location,sqft,bath,bhk, modl):
+
+    with open("columns.json", "r") as file:
+        col = json.load(file)
+
+    location_col = col['data_columns']
+
+    if location.lower() in location_col:
+        loc_index = location_col.index(location.lower())
+    #print(type(loc_index))
+    x = np.zeros(len(location_col))
+    #print(x)
+    x[0] = sqft
+    #print(x)
+    x[1] = bath
+    #print(x)
+    x[2] = bhk
+    #print(x)
+    if loc_index >= 0:
+        #print(loc_index)
+        x[loc_index] = int(1)
+    #x_df = pd.DataFrame([x], columns=location_col)
+
+    #print(x)
+    return modl.predict([x])[0]
+
+def call_pred(location,sqft,bath,bhk):
+
+    with open("banglore_home_prices_model.pickle", "rb") as f:
+        modl = pickle.load(f)
+
+    price_pred = predict_price(location,sqft,bath,bhk, modl)
+
+    return print(round(price_pred,2))
+
+
+
+call_pred("2nd phase judicial layout", 1600, 2, 2)
